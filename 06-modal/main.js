ed6bc8dd7684610b3c6dc9312a26201451b4c4ac
Bartlomiej Przymus <bartlomiej.przymus@gmail.com>
2018-12-18 20:02:39 +0000

    Added new project model element

---
+++ 06-modal/main.js
@@ -0,0 +1,32 @@
+Vue.component('comp-name', {
+
+    props: [props],
+
+    data() {
+
+        return {
+
+        };
+
+    },
+
+    template: `
+    
+    <!-- Template Goes Here -->
+    
+    `,
+
+    methods: {
+
+        
+
+    }
+
+});
+
+
+new Vue({
+
+el: '#root'
+
+});
