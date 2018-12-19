67b9e50aa2c8fa3b8043129051d4cb609c61818a
Bartlomiej Przymus <bartlomiej.przymus@gmail.com>
2018-12-19 22:39:24 +0000

    Tabs in vue exercise complete

--- 07-tabs/main.js
+++ 07-tabs/main.js
@@ -4,7 +4,7 @@ Vue.component('tabs', {
     <div class="tabs">
         <ul>
             <li v-for="tab in tabs" :class="{'is-active' : tab.isActive}">
-                <a href="#" @click="selectTab(tab)">{{ tab.name }}</a>
+                <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
             </li>
         </ul>
     </div>
@@ -31,7 +31,7 @@ Vue.component('tabs', {
 });
 Vue.component('tab', {
     template: `
-    <div><slot></slot></div>
+    <div v-show="isActive"><slot></slot></div>
 `,
     props:  {
         name: {required: true},
@@ -42,9 +42,14 @@ Vue.component('tab', {
             isActive: false
         };
     },
+
+    computed: {
+        href() { return '#' + this.name.toLowerCase().replace(/ /g, '-')}
+    },
+
     mounted() {
         this.isActive = this.selected;
-    },
+    }
 });
 new Vue({
     el: '#root'
