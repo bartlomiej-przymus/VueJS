2e5715483ce80cabd24b6c39d3cfbc00132b1f2c
Bartlomiej Przymus <bartlomiej.przymus@gmail.com>
2018-12-19 08:33:35 +0000

    Added new exercise to display modal custom component.

--- 06-modal/main.js
+++ 06-modal/main.js
@@ -1,32 +1,19 @@
-Vue.component('comp-name', {
-
-    props: [props],
-
-    data() {
-
-        return {
-
-        };
-
-    },
-
+Vue.component('modal', {
     template: `
-    
-    <!-- Template Goes Here -->
-    
-    `,
-
-    methods: {
-
-        
-
-    }
-
+    <div class="modal is-active">
+            <div class="modal-background"></div>
+            <div class="modal-content">
+                <div class="box">
+                    <slot></slot>
+                </div>
+            </div>
+            <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+    </div>
+    `
 });
-
-
 new Vue({
-
-el: '#root'
-
+    el: '#root',
+    data: {
+        showModal: false
+    }
 });
