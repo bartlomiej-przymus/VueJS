a022a91365dfe98fddcd82447e377d1351eee03f
Bartlomiej Przymus <bartlomiej.przymus@gmail.com>
2018-12-20 14:14:56 +0000

    Added new proper vue.js app setup exercise. Used Vue cli and webpack to start new sample project. Create one new custom component.

---
+++ 12-webpack-vue-cli/my-app/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <message>Hello There!</message>
+  </div>
+</template>
+
+<script>
+import message from "./components/message.vue";
+
+export default {
+  name: 'app',
+  components: {message},
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 12-webpack-vue-cli/my-app/src/components/message.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="box">
+        <p>
+            <slot></slot>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.box {background: grey; padding: 10px; border: 1px solid #c5c5c5}
+</style>
