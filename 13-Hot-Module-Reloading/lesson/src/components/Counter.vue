a815da27296679520cb89fbf8fca9d8db8bc35e1
Bartlomiej Przymus <bartlomiej.przymus@gmail.com>
2018-12-21 10:42:27 +0000

    Added siple exercise to see Hot module reloading in action!

---
+++ 13-Hot-Module-Reloading/lesson/src/components/Counter.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>The Count: {{ count }}</h2>
+    <p>
+        <!-- We can change value of count increment live and old data will remain! -->
+        <button @click="count += 20">Increment</button>
+    </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        };
+    }
+}
+</script>
+
+<style>
+
+</style>
