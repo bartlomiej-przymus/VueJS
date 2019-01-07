738540d302de6f412b7fff6a4f349bf558a40068
Bartlomiej Przymus <bartlomiej.przymus@gmail.com>
2019-01-07 14:21:56 +0000

    Refactored code added comments exposed inner form to root Vue element by wrapping submit method in new custom promise.

--- 15-Object-Oriented-Forms-using-Vue-and-Laravel/vue-form/public/js/app.js
+++ 15-Object-Oriented-Forms-using-Vue-and-Laravel/vue-form/public/js/app.js
@@ -1,15 +1,53 @@
 class Errors {
+
+    /**
+     * Create a new Errors Instance.
+     */
     constructor() {
         this.errors = {}
     }
+
+    /**
+     * Determine if an errors exist for the given field.
+     * 
+     * @param {string} field 
+     */
+    has(field) {
+        return this.errors.hasOwnProperty(field);
+    }
+
+    /**
+     * Determine if we have any errors.
+     */
+    any() {
+        return Object.keys(this.errors).length > 0;
+    }
+
+    /**
+     * Retrieve the error message for the field.
+     * 
+     * @param {string} field 
+     */
     get(field) {
         if (this.errors[field]) {
-           return this.errors[field][0];
+            return this.errors[field][0];
         }
     }
+
+    /**
+     * Record the new errors
+     * 
+     * @param {object} errors 
+     */
     record(errors) {
-        this.errors = errors;
+        this.errors = errors.errors;
     }
+
+    /**
+     * Clear one or all error fields.
+     * 
+     * @param {string|null} field 
+     */
     clear(field) {
         if(field) {
             delete this.errors[field];
@@ -17,48 +55,103 @@ class Errors {
         }
         this.errors = {};
     }
-    has(field) {
-        return this.errors.hasOwnProperty(field);
-    }
-    any() {
-        return Object.keys(this.errors).length > 0;
-    }
 }
 
 class Form {
+    /**
+     * Create a new Form instance.
+     * 
+     * @param {object} data 
+     */
     constructor(data) {
         this.originalData = data;
+
         for (let field in data) {
             this[field] = data[field];
         }
+
         this.errors = new Errors()
     }
+
+    /**
+     * Fetch all relevant data for the form.
+     */
     data() {
-        let data = Object.assign({}, this); //{name, description, originalData, errors}
-        delete data.originalData;
-        delete data.errors;
+        let data = {};
+        for (let property in this.originalData) {
+            data[property] = this[property];
+        }
+        // let data = Object.assign({}, this); //{name, description, originalData, errors}
+        // delete data.originalData;
+        // delete data.errors;
         return data;
     }
+
+    /**
+     * Reset the form fields.
+     */
     reset(){
-        for(let field in originalData) {
+        for(let field in this.originalData) {
             this[field] = '';
         }
+        this.errors.clear();
 
     }
-    submit(requestType, url) {
-        //axios can be here now
-        axios[requestType](url, this.data())
-            .then(this.onSuccess.bind(this))
-            .catch(this.onFail.bind(this))
+
+    post(url) {
+        return this.submit('post', url);
     }
-    onSuccess(response) {
+
+    delete(url) {
+        return this.submit('delete', url);
+    }
+
+    /**
+     * Submit the form.
+     * 
+     * @param {string} requestType 
+     * @param {string} url 
+     */
+    submit(requestType, url) {
+        return new Promise((resolve, reject)=>{
+            axios[requestType](url, this.data())
+            //if i have succesful response
+            .then(response => {
+                this.onSuccess(response.data); //we delegate to onSuccess method and we give it data from our response
+
+                resolve(response.data);
+            })
+            .catch(error => {
+                this.onFail(error.response.data);
+
+                reject(error.response.data);
+            });
+            // .then(this.onSuccess.bind(this))
+            // .catch(this.onFail.bind(this))
+
+        })
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+    }
+
+    /**
+     * Handle a successful form submission
+     * 
+     * @param {object} data 
+     */
+    onSuccess(data) {
         //temp message
-        alert(response.data.message);
-        this.errors.clear();
+        alert(data.message);
         this.reset();
     }
-    onFail(error) {
-        this.errors.record(error.response.data.errors);
+
+    /**
+     * Handle a failed form submission
+     * 
+     * @param {object} errors
+     */
+    onFail(errors) {
+        this.errors.record(errors);
     }
 }
 
@@ -71,15 +164,18 @@ new Vue({
             description: ''
         })
     },
-    // data: {
-    //     name: '',
-    //     description: '',
-    //     errors: new Errors()
-    // },
 
     methods: {
         onSubmit() {
-            this.form.submit('post', '/projects')
+            this.form.post('/projects');
+              
+
+            // this.form.submit('post', '/projects')
+            //     .then(data => console.log(data))
+            //     .catch(error => console.log(error));
+            //we can handle different crud functionality by calling newly defined methods on form class
+            //this.form.delete('/projects');
+            //or
         }
     }
 });
