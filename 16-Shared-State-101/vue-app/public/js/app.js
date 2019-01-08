39770ee84e0b35ee377d067c446b7e29e6078ee1
Bartlomiej Przymus <bartlomiej.przymus@gmail.com>
2019-01-08 12:39:19 +0000

    practiced simple concept of shared state in VueJS.

---
+++ 16-Shared-State-101/vue-app/public/js/app.js
@@ -0,0 +1,22 @@
+let store = {
+    user: {
+        name: 'John Doe'
+    }
+};
+
+new Vue({
+    el: '#one',
+
+    data: {
+        foo: 'bar',
+        shared: store
+    }
+});
+new Vue({
+    el: '#two',
+
+    data: {
+        foo: 'other',
+        shared: store
+    }
+});
