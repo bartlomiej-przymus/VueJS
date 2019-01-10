8a680dcf8e30c519fe226965fe45bda25c0d7bea
Bartlomiej Przymus <bartlomiej.przymus@gmail.com>
2019-01-10 13:42:40 +0000

    Front end and setup done in my first SPA using VueJS and Laravel

---
+++ 18-Vue-SPA/stream/resources/js/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Home Page</div>
+
+                    <div class="panel-body">
+                        I'm an example component.
+                        <!-- you render data here {{ body }}-->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() { //can be created()
+            //if you need to fetch something from database you put your ajax requests here like:
+            //axios. 
+            console.log('Component mounted.')
+        }
+    }
+</script>
